44654bddfd9b3c74a3b8ec38d04df7aae8bee465
Dario Domizioli <dario.domizioli@gmail.com>
2016-08-13 00:23:52 +0100

    Starting to code the content generation

--- src/structure.rs
+++ src/structure.rs
@@ -1,5 +1,8 @@
 use rustc_serialize::json;
 
+use std::io::prelude::*;
+use std::fs::File;
+
 #[derive(Clone, PartialEq, RustcDecodable, RustcEncodable)]
 pub struct Chapter {
     title: String,
@@ -18,53 +21,111 @@ pub struct Structure {
     parts: Vec<Part>
 }
 
-
 impl Structure {
     pub fn from_json(js: &str) -> Structure {
         json::decode::<Structure>(js).unwrap()
     }
-
-    pub fn get_all_files(&self) -> Vec<String> {
-        self.parts.iter().map(|ref part| {
-            part.chapters.iter().map(|ref chap| {
-                &chap.files
-            }).fold(Vec::new(), |mut acc, f| {
-                acc.extend_from_slice(&f[..]); acc
-            })
-        }).fold(Vec::new(), |mut acc, f| {
-            acc.extend_from_slice(&f[..]); acc
-        })
-    }
 }
 
 
+pub struct Content {
+    toc: String,
+    chunks: Vec<String>
+}
 
-#[test]
-fn test_structure_1() {
-    let js = r#"
-    {
-        "title": "My Book",
-        "parts": [{
-            "title": "Part 1",
-            "chapters": [{
-                "title": "Chap 1",
-                "files": ["alpha", "beta"]
-            }, {
-                "title": "Chap 2",
-                "files": ["gamma"]
-            }]
-        }, {
-            "title": "Part 2",
-            "chapters": [{
-                "title": "Chap 3",
-                "files": ["delta"]
-            }, {
-                "title": "Chap 4",
-                "files": ["epsilon", "eta"]
-            }]
-        }]
-    }"#;
-    let st = Structure::from_json(&js);
-    let fs = st.get_all_files();
-    assert!(fs.len() == 6);
+impl Content {
+    fn build_toc(st: &Structure) -> Result<String, String> {
+        let mut toc = String::new();
+        toc = toc + r#"<div class="toc">"# + "\n\n";
+        let mut part_index = 1;
+        for part in st.parts.iter() {
+            let part_link = format!(
+                "**[{0} {1}](#kos_ref_part_{0})**\n\n", part_index, part.title);
+            toc = toc + &part_link;
+            let mut chap_index = 1;
+            for chap in part.chapters.iter() {
+                let chap_link = format!(
+                    "*[{0}.{1} {2}](#kos_ref_chap_{0}_{1})*\n\n",
+                    part_index, chap_index, chap.title);
+                toc = toc + &chap_link;
+                chap_index += 1;
+            }
+            part_index += 1;
+        }
+        toc = toc + "</div>\n\n";
+        Ok(toc)
+    }
+
+    fn build_chunks(st: &Structure) -> Result<Vec<String>, String> {
+        let mut chunks = Vec::new();
+        let book_header = 
+                r#"<div class="book_title">"#.to_string() +
+                r#"<a id="kos_book_title"></a><p class="book_title">"# +
+                &st.title +
+                "</p></div>\n\n";
+            chunks.push(book_header);
+        let mut part_index = 1;
+        for part in st.parts.iter() {
+            let part_header = 
+                r#"<div class="part_title">"#.to_string() + 
+                r#"<a id="kos_ref_part_"# +
+                &format!("{}", part_index) +
+                r#""></a><p class="part_title">"# +
+                &part.title +
+                "</p></div>\n\n";
+            chunks.push(part_header);
+            let mut chap_index = 1;
+            for chap in part.chapters.iter() {
+                let chap_header = 
+                    r#"<div class="part_title">"#.to_string() +
+                    r#"<a id="kos_ref_chap_"# +
+                    &format!("{}", part_index) +
+                    "_" +
+                    &format!("{}", chap_index) +
+                    r#""></a><h1>"# +
+                    &chap.title +
+                    "</h1></div>\n\n";
+                chunks.push(chap_header);
+                for f in chap.files.iter() {
+                    let file_content = match File::open(f) {
+                        Ok(mut fread) => {
+                            let mut res = String::new();
+                            match fread.read_to_string(&mut res) {
+                                Ok(_) => (),
+                                Err(_) => {
+                                    return Err(
+                                        "Error reading file ".to_string() +
+                                        f + "!\n");
+                                }
+                            }
+                            res
+                        },
+                        Err(_) => {
+                            return Err(
+                                "Error reading file ".to_string() + f + "!\n");
+                        }
+                    };
+                    chunks.push(file_content);
+                }
+                chap_index += 1;
+            }
+            part_index += 1;
+        }
+        Ok(chunks)
+    }
+
+    pub fn from_structure(st: &Structure) -> Result<Content, String> {
+        let toc = Content::build_toc(st);
+        let chunks = Content::build_chunks(st);
+        Ok(Content {
+            toc: match toc {
+                Ok(t) => t,
+                Err(e) => { return Err(e); }
+            },
+            chunks: match chunks {
+                Ok(c) => c,
+                Err(e) => { return Err(e); }
+            }
+        })
+    }
 }
